cd1214abbe9c9ea2303f1b2d2d4ee6993b0a00d2
joancruzz <joancruz95@gmail.com>
2019-06-21 06:23:10 +0000

    Program that prints numbers 00-99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+* main - Print 0 to 99 with commas
+* Return: 0
+*/
+
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != '9' || m != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+}
