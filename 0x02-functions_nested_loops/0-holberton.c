84a805896dfbacc402419c210d1912fee01eec3a
joancruzz <joancruz95@gmail.com>
2019-06-24 23:12:06 +0000

    Print Holberton with printchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
 #include "holberton.h"
+
+/**
+* main - entry point
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	putchar("Holberton\n");
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar(10);
+
 	return(0);
 }
