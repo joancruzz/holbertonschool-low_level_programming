83e64b1b9c2b56d394361e51264a273724d20544
joancruzz <joancruz95@gmail.com>
2019-06-26 05:37:39 +0000

    Create program to add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - add two numbers
+ *@i: The first number to be added
+ *@j: The second number to be added
+ *Return: sum of two numebrs
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
