b4cbe2131acdf9f49c607dfffd797fc3f86203e2
joancruzz <joancruz95@gmail.com>
2019-06-27 19:57:05 +0000

    Create program to print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Entry
+ *
+ *Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+
+	{
+		_putchar (i);
+	}
+
+	_putchar ('\n');
+}
