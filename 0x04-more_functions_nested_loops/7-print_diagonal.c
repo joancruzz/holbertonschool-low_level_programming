d0152b91d08f38f3d7f9f80fdee1b69753707492
joancruzz <joancruz95@gmail.com>
2019-06-28 02:25:19 +0000

    Create square with sign

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  *
+  *
+  *
+  */
+
+void print_diagonal(int n);
+{
+	int x;
+	int y;
+	int space;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar ('\');
+	}
+	_putchar ('\n');
+}
