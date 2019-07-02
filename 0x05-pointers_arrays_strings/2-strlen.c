10c0bd648508ecd0789e7f94fcade76bb848ffb7
joancruzz <joancruz95@gmail.com>
2019-07-02 00:11:08 +0000

    Return length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
-  *
-  *
-  *
+  * _strlen - Entry
+  * @s: pointer of string
+  * Return: i
   */
 
 int _strlen(char *s)
 {
+	int i = 0;
+
+	for (; *s != 0; s++)
+	{
+		i++;
+	}
+
+	return (i);
+}
+
 
