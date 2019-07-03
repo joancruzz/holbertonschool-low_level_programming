313f3cc6641910b9a827abd2be8fd50abec365e6
joancruzz <joancruz95@gmail.com>
2019-07-03 05:49:45 +0000

    Print two chars in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
 /**
-  *
-  *
-  *
-  */
+ * puts2 - Print 2 char out of string
+ * @str: input string
+ * Return: void
+ */
 
-void puts2(char *str);
-
+void puts2(char *str)
 {
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
