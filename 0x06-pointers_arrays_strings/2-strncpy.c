ae9e92f26029948243b54047f8301dd53f05a51e
joancruzz <joancruz95@gmail.com>
2019-07-05 23:24:28 +0000

    Create function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,24 @@
 #include "holberton.h"
 
 /**
- *  - Entry point
- *
- *Return: 
+ * _strncpy - Entry point
+ * @dest: cat
+ * @src: cat
+ * @n: counter
+ *Return: dest
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = 0;
+	}
+	return (dest);
+}
