adc74e22415084ac2508c67ed31e75fed696ee42
joancruzz <joancruz95@gmail.com>
2019-07-05 22:24:55 +0000

    Concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,28 @@
 #include "holberton.h"
 
 /**
- *  - Entry point
- *
- *Return: 
+ * _strncat - Entry point
+ * @dest: cat
+ * @src: cat
+ * @n: counter
+ *Return: dest
  */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	char *temp = dest;
+	int i;
+
+	while (*temp)
+	{
+		temp++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
+	*temp = 0;
+	return (dest);
+}
