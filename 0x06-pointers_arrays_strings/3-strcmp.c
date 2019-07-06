f0d2d8d6407b52a28a9cf3679dc9f4d9bf15839e
joancruzz <joancruz95@gmail.com>
2019-07-06 01:16:34 +0000

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,22 @@
 #include "holberton.h"
 
 /**
- *  - Entry point
- *
- *Return: 
+ * _strcmp  - Entry point
+ * @s1: compare to s2
+ * @s2: compare to s1
+ *Return: 0x
  */
 
+int _strcmp(char *s1, char *s2)
+{
+	int result;
 
+	for (result = 0; s1[result] == s2[result] && s1[result] == 0; result++)
+
+	if (s1[result] < s2[result])
+		return (-15);
+	else if (s1[result] > s2[result])
+		return (15);
+	else
+		return (result);
+}
