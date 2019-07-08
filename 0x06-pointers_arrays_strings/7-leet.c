df5ebfc9b973d566c7453f1887f7d53204d36598
joancruzz <joancruz95@gmail.com>
2019-07-08 05:37:03 +0000

    Encode string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,29 @@
 #include "holberton.h"
 
 /**
- *  - Entry point
- *
- *Return: 
+ * leet - Entry point
+ * @s: string
+ * Return: s
  */
 
+char *leet(char *s)
+{
+	int x, y;
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	char new[] = {'4', '3', '0', '7', '1'};
 
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y <= 4; y++)
+
+		{
+			if (s[x] == lower[y] || s[x] == upper[y])
+			{
+				s[x] = new[y];
+			}
+		}
+	}
+
+	return (s);
+}
