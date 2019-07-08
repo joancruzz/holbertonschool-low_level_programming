e134299f8bd7e546781a724f52ebf96b367e66c6
joancruzz <joancruz95@gmail.com>
2019-07-08 04:14:20 +0000

    Capitalize words on strinf

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,26 @@
 #include "holberton.h"
 
 /**
- *  - Entry point
- *
- *Return: 
+ * cap_string - Entry point
+ * @s: string
+ * Return: s
  */
 
+char *cap_string(char *s)
+{
+	int x, y;
 
+	char sym[] = " ,:.!?\"(){}\t\n";
+
+	for (x = 0; s[x] != 0; x++)
+	{
+		for (y = 0; s[x] > 'a' && s[x] < 'z' && sym[y] != 0; y++)
+		{
+			if (s[x - 1]  == sym[y] || s[x] == s[0])
+		{
+			s[x] = s[x] - 32;
+		}
+		}
+	}
+	return (s);
+}
