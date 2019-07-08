253a269caf7b6a5c680f75084066f9fbfff4c82c
joancruzz <joancruz95@gmail.com>
2019-07-08 03:21:19 +0000

    Change lowercase string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,22 @@
 #include "holberton.h"
 
 /**
- *  - Entry point
- *
- *Return: 
+ * string_toupper - Entry point
+ * @s: String
+ *Return: s
  */
+
+char *string_toupper(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != 0; x++)
+	{
+		if (s[x] >= 'a' && s[x] <= 'z')
+			s[x] = s[x] - 32;
+		;
+	}
+	return (s);
+}
 
 
