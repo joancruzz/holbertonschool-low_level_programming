80fa37574cab39d32e6b7f11d83f0b4ff3087061
joancruzz <joancruz95@gmail.com>
2019-07-08 06:07:40 +0000

    Encode string

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,9 +1,26 @@
 #include "holberton.h"
-
 /**
- *  - Entry point
- *
- *Return: 
+ * rot13 - Change string into rot13
+ * @s: String
+ * Return: s
  */
 
+char *rot13(char *s)
+{
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int x, y;
 
+	for (x = 0; s[x] != 0; x++)
+	{
+		for (y = 0; upper[y] != 0; y++)
+		{
+			if (s[x] == upper[y])
+			{
+				s[x] = rot13[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
