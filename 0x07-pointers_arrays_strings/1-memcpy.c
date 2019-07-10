d2ea4d97c7866c2366cf74d3a169d2bce7735656
joancruzz <joancruz95@gmail.com>
2019-07-10 04:25:44 +0000

    Create fuunction to copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,19 @@
-i
+#include "holberton.h"
+
+/**
+ *  _memcpy - copy memory area
+ * @dest: pointer for memory area dest
+ * @src: pointer for memory area src
+ * @n: variable to copy bytes
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
