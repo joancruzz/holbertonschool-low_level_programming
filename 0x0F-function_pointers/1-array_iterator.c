7d688f88419a4a3a0a68db5a189127850395d215
joancruzz <joancruz95@gmail.com>
2019-07-25 05:15:02 +0000

    Updated header files and initial commit for 1-array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function that executes a function on each element of an array
+ * @array: Pointer of array
+ * @size: Size of the integer array
+ * @action: Pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	if (array != NULL && action != NULL)
+	{
+		for (x = 0; x < size; x++)
+			action(array[x]);
+	}
+}
