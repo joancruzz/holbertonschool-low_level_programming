34f8a374f983bc8680e596195c79902081879d20
joancruzz <joancruz95@gmail.com>
2019-07-25 05:16:52 +0000

    Erase redundant characters

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,6 @@
 #include "function_pointers.h"
-
 /**
- * array_iterator - Function that executes a function on each element of an array
+ * array_iterator - Function that executes a function on each element of array
  * @array: Pointer of array
  * @size: Size of the integer array
  * @action: Pointer to the function
