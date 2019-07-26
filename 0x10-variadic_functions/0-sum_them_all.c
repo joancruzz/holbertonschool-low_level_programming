9983e229054d798ca3d82d44fa8ba0b48ea42852
joancruzz <joancruz95@gmail.com>
2019-07-26 19:56:45 +0000

    Add commenting to code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,23 +2,22 @@
 
 /**
   * sum_them_all - Returns the sum of all parameters
-  * @n: Constant number of elements
+  * @n: Constant number of elements in the argument
   * Return: sum
   */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
-	va_list arguments;
-	unsigned int i;
+	unsigned int sum = 0; /*sum of all arguments*/
+	va_list arguments; /*numbers that will be added*/
+	unsigned int i; /*counter for arguments*/
 
 	if (n == 0)
 		return (0);
 	va_start(arguments, n);
 
-	for (i = 0; i < n; i++)
-		sum += va_arg (arguments, unsigned int);
-
-	va_end (arguments);
-	return(sum);
+	for (i = 0; i < n; i++) /*count for elements(n) of argument*/
+		sum += va_arg(arguments, unsigned int);
+	va_end(arguments);
+	return (sum);
 }
