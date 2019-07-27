b254e8ecf6e1e6386024b7343a28f9fe42e3790a
joancruzz <joancruz95@gmail.com>
2019-07-27 22:34:47 +0000

    Write function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+/**
+ * _printchar - function that prints character
+ * @arguments: data type held in va_list to print
+ */
+void _printchar(va_list arguments)
+{
+	printf("%c", va_arg (arguments, int));
+}
+
+
+/**
+ * _printint - function that prints integer
+ * @arguments: data type held in va_list to print
+ */
+void _printint(va_list arguments)
+{
+	printf("%i", va_arg (arguments, int));
+}
+
+/**
+ * _printfloat - function that prints float
+ * @arguments: data type held in va_list to print
+ */
+void _printfloat(va_list arguments)
+{
+	printf("%f", va_arg (arguments, double));
+}
+
+/**
+ * _printstr - function that prints string
+ * @arguments: data type help in va_list to print
+ */
+void _printstr(va_list arguments)
+{
+	char *str;
+	str = va_arg (arguments, char*);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - function that prints all datatypes
+ * @format: array of chars for the data types
+ */
+void print_all(const char * const format, ...)
+{
+
+	unsigned int x = 0;
+	int y = 0;
+	char *separator = ", ";
+	va_list arguments;
+
+	datatypes_t types[] = {
+		{"c", _printchar},
+		{"i", _printint},
+		{"f", _printfloat},
+		{"s", _printstr},
+		{NULL, NULL}
+	};
+
+	va_start(arguments, format);
+
+	while (format != NULL && format[x])
+	{
+		y = 0;
+		while (types[y].argtype)
+		{
+			if (format[x] == *types[y].argtype)
+			{
+				types[y].funcpointer (arguments);
+				if (format[x + 1] != 0)
+					printf("%s", separator);
+			}
+			y++;
+		}
+		x++;
+	}
+	printf("\n");
+	va_end(arguments);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,9 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * struct datatypes - struct to use group all datatypes
+  * @argtype: the type of argument
+  * @funcpointer: function pointer to print function
+  */
+
+typedef struct datatypes
+{
+	char *argtype;
+	void (*funcpointer)(va_list argument);
+}datatypes_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-
-#endif
+#endif /*VARIADIC_FUNCTIONS_H*/
