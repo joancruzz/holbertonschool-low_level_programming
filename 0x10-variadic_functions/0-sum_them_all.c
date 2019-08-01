d4d7b250d18ee8520bfcab47a461a9337af1c6d0
joancruzz <joancruz95@gmail.com>
2019-08-01 05:17:35 +0000

    Edit unsigned int

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,9 @@
 #include "variadic_functions.h"
-
 /**
   * sum_them_all - Returns the sum of all parameters
   * @n: Constant number of elements in the argument
   * Return: sum
   */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0; /*sum of all arguments*/
@@ -17,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arguments, n);
 
 	for (i = 0; i < n; i++) /*count for elements(n) of argument*/
-		sum += va_arg(arguments, unsigned int);
+		sum += va_arg(arguments, int);
 	va_end(arguments);
 	return (sum);
 }
