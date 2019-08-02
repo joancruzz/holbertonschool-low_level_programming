a6234df523779441ac4cdc0c495cc10827a4b097
joancruzz <joancruz95@gmail.com>
2019-08-02 00:48:01 +0000

    Add comments

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * print_list - function that prints elements of a list
+ * @h: pointer to str
+ * Return: counter, num elements in  list
  */
 
 size_t print_list(const list_t *h)
