9fc3baf03b0162a418c8f3b451935346274e8964
joancruzz <joancruz95@gmail.com>
2019-08-07 03:30:24 +0000

    Add else statement after if

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (temp)
 			temp = temp->next;
-		return (NULL);
+		else
+			return (NULL);
 	}
 	new = malloc(sizeof(listint_t));
 	if (!new)
