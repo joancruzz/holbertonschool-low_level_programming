6bb717f95c8416e0f712e944b907801e14d23f96
joancruzz <joancruz95@gmail.com>
2019-08-09 05:20:30 +0000

    Add new prototype

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,8 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -9,5 +11,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _putchar(char c);
 
 #endif
