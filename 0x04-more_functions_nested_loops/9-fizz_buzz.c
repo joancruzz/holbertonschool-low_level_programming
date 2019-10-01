4bb95a97d1bb12e21f0a84b06057f3f88c592b5f
joancruzz <joancruz95@gmail.com>
2019-10-01 05:53:46 +0000

    Edit if statement

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
   * main - Entry of program
@@ -11,15 +11,15 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz ");
+		else if (num % 3 == 0)
 			printf("Fizz ");
 		else if (num % 5 == 0)
 			printf("Buzz ");
-		else if (num % 15 == 0)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", num);
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
