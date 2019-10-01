3ba7b73261d336a599932a5337f267ef15297a61
joancruzz <joancruz95@gmail.com>
2019-10-01 06:49:44 +0000

    Edit variable declarations

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,10 +6,15 @@
  */
 void print_triangle(int size)
 {
-	int x, y, spaces = 0;
+	int x;
+	int y;
+	int spaces = 0;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+		return;
+	}
 	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size; y++)
