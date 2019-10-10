4dec6ee32526344d7af037501a46b81a81288ddd
joancruzz <joancruz95@gmail.com>
2019-10-10 23:56:09 +0000

    Djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,17 @@
+#include "hash_tables_h"
+/**
+  * hash_djb2 - implement djb2 algorithm
+  * @str: string
+  * Return: hash
+  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return (hash);
 }
