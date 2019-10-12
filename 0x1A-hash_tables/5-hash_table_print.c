cec15d3ea1846360177d61548fb0bfd25c74da4c
joancruzz <joancruz95@gmail.com>
2019-10-12 00:37:03 +0000

    Print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ * Return: hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *tmp;
+	int comma = 0;
+	unsigned long int index = 0;
 
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (index < ht->size)
+	{
+		tmp = ht->array[index];
+		while (tmp)
+		{
+			if (comma != 0)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			comma = 1;
+		}
+		index++;
+	}
+	printf("}\n");
 }
