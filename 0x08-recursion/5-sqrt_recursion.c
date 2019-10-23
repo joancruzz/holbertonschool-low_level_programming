4aed8e80206d876c5eca3065ce18c864ab87d28e
joancruzz <joancruz95@gmail.com>
2019-10-23 07:31:56 +0000

    find swrt of num

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,34 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - Square root function
- * @x: Any number to square root
- */
-
-int sqrt(int x, int root)
-{
-	int square = root * root;
-
-	if (square < x)
-		return (sqrt(x, root + 1));
-	else if (square > x)
-		return (-1);
-	else
-		return (root);
-}
-
-/**
- * _sqrt_recursion - Square root function
- * @n: N
- * Return: Square root if perfect, if not -1
+ * _sqrt_recursion - Determine the natural square root of a number
+ * @n: number to sqroot
+ * Return: The square root of n
  */
 int _sqrt_recursion(int n)
 {
-	return (find_sqrt(n, 0));
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (n);
+
+	return (find_sqrt(n, 1));
+}
+
+/**
+ * find_sqrt - Find the natural square root of a number
+ * @n: The number
+ * @i: The number to check
+ *
+ * Return: The square root if it exists, otherwise -1
+ */
+int find_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+	else
+		return (find_sqrt(n, i + 1));
 }
