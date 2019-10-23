7ad5c8d5c002febc728454f3305a0468141a2fb4
joancruzz <joancruz95@gmail.com>
2019-10-23 06:57:03 +0000

    Change for loop to while loop

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,14 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 
-	for (; ; haystack++)
+	while (*haystack)
 	{
 		i = 0;
 		while (haystack[i] && needle[i] && haystack[i] == needle[i])
 			i++;
 		if (!needle[i])
 			return (haystack);
+		haystack++;
 	}
 	return (0);
 }
